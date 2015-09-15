a4f2f2249bbb950cdc450d7a641e2fc94c93c209
Colin Walters <walters@verbum.org>
2015-09-15 13:29:53 -0400

    resolve: Support source-less dist-git
    go-srpm-macros in Fedora is like this.

--- rdgo/task_resolve.py
+++ rdgo/task_resolve.py
@@ -160,23 +160,22 @@ class TaskResolve(Task):
 
         [rpm_version, rpm_release] = self._rpm_verrel(component, upstream_tag, upstream_rev, distgit_desc)
 
+        spec_fn = specfile.spec_fn(spec_dir=distgit_co)
+        spec = specfile.Spec(distgit_co + '/' + spec_fn)
+
         if upstream_desc is not None:
             tar_dirname = '{0}-{1}'.format(component['name'], upstream_desc)
             tarname = tar_dirname + '.tar.gz'
             tmp_tarpath = distgit_co + '/' + tarname
             self._tar_czf_with_prefix(upstream_co, tar_dirname, tmp_tarpath)
-        spec_fn = specfile.spec_fn(spec_dir=distgit_co)
-        spec = specfile.Spec(distgit_co + '/' + spec_fn)
-        has_zero = spec.get_tag('Source0', allow_empty=True) is not None
-        source_tag = 'Source'
-        if has_zero:
-            source_tag += '0'
-        if upstream_desc is not None:
+            has_zero = spec.get_tag('Source0', allow_empty=True) is not None
+            source_tag = 'Source'
+            if has_zero:
+                source_tag += '0'
             spec.set_tag(source_tag, tarname)
-        spec.set_tag('Version', rpm_version)
-        spec.set_tag('Release', rpm_release + '%{?dist}')
-        if upstream_desc is not None:
+            spec.set_tag('Version', rpm_version)
             spec.set_setup_dirname(tar_dirname)
+        spec.set_tag('Release', rpm_release + '%{?dist}')
         # Anything useful there you should find in upstream dist-git or equivalent.
         spec.delete_changelog()
         # Forcibly override
