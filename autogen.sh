19bc0217f7f39f8ead34f237993e0f81919275da
Geoff Levand <geoff@infradead.org>
2018-09-04 16:38:12 -0700

    autogen.sh: Check for installed programs
    Add checks for programs used by configure.
    Also changes from using 'which' to using the shell
    built-in 'command'.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 set -e
 
+progs="autoconf autoreconf automake libtoolize pkg-config"
+
+for p in ${progs}; do
+	if ! test -x "$(command -v ${p})"; then
+		echo "*** Please install ${p}"
+		result=1
+	fi
+done
+
+[ -z ${result} ] || exit 1
+
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 olddir=`pwd`
 cd $srcdir
-
-AUTORECONF=`which autoreconf`
-if test -z $AUTORECONF; then
-        echo "*** No autoreconf found, please intall it ***"
-        exit 1
-fi
 
 mkdir -p m4
 
